ac8fb4ec4aabb75d72e553f6d1ef4a2d7af88a15
nguyenvietdoanh0502 <vietdoanh0502@gmail.com>
2025-10-26 23:59:00 +0700

    FixBTVN2

--- Tuan2/BTVN/Bai1/src/Main.java
+++ Tuan2/BTVN/Bai1/src/Main.java
@@ -1,54 +1,7 @@
 import java.util.Scanner;
 
 public class Main {
-    public static int Sum(int[] array){
-        int sum = 0;
-        for(int i =0;i<array.length;i++){
-            sum+=array[i];
-        }
-        return sum;
-    }
-    public static void Sort(int[] array){
-        for(int i = 0;i<array.length-1;i++){
-            for(int j=i+1;j<array.length;j++){
-                if(array[i]>array[j]){
-                    int temp = array[i];
-                    array[i]=array[j];
-                    array[j]=temp;
-                }
-            }
-        }
-    }
-    public static void MaxMin(int[] array){
-        Sort(array);
-        System.out.println("Giá trị lớn nhất của mảng là: "+array[array.length-1]);
-        System.out.println("Giá trị nhỏ nhất của mảng là: "+array[0]);
-    }
-    public static boolean CheckSNT(int x){
-        if (x<=1){
-            return false;
-        }
-        for(int i =2;i<Math.sqrt(x);i++){
-            if(x%i==0){
-                return false;
-            }
-        }
-        return true;
-    }
-    public static void MaxSNT(int[] array){
-        Sort(array);
-        boolean check = true;
-        for(int i = array.length-1;i>=0;i--){
-            if(CheckSNT(array[i])){
-                System.out.println("Số nguyên tố lớn nhất là: "+array[i]);
-                check = false;
-                break;
-            }
-        }
-        if(check){
-            System.out.println("Không có số nguyên tố nào");
-        }
-    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Nhập số lượng phần tử: ");
@@ -68,20 +21,20 @@ public class Main {
         int choose = sc.nextInt();
         switch (choose){
             case 1:
-                System.out.println("Tổng các phần tử trong mảng là: "+Sum(array));
+                System.out.println("Tổng các phần tử trong mảng là: "+Utils.Sum(array));
                 break;
             case 2:
-                MaxMin(array);
+                Utils.MaxMin(array);
                 break;
             case 3:
-                Sort(array);
+                Utils.Sort(array);
                 System.out.print("Mảng sau khi sắp xếp: ");
                 for(int i =0;i<n;i++){
                     System.out.print(array[i]+" ");
                 }
                 break;
             case 4:
-                MaxSNT(array);
+                Utils.MaxSNT(array);
                 break;
             default:
                 System.out.println("Lựa chọn nhập sai");
--- Tuan2/TrenLop/Bai1/src/Mang.java
+++ Tuan2/TrenLop/Bai1/src/Mang.java
@@ -1,4 +1,8 @@
-package PACKAGE_NAME;
-
 public class Mang {
+    static String[] s = {"Xin","Chao","The","Gioi","Nay"};
+    public static void Print(){
+        for(String x: s){
+            System.out.println(x);
+        }
+    }
 }
